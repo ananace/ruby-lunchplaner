c3f68d37b4be3460a16cb4e22f9d149f583cab4d
Alexander Olofsson <alexander.olofsson@liu.se>
2017-11-02 10:10:15 +0100

    Remove all newlines from the menu

--- lib/lunchplaner/backends/universitetsklubben.rb
+++ lib/lunchplaner/backends/universitetsklubben.rb
@@ -4,7 +4,7 @@ module Lunchplaner
       url 'http://www.hors.se/restaurang/universitetsklubben/'
 
       def daily
-        data.css('.hors-menu .row .text-left').map { |e| e.content.strip }
+        data.css('.hors-menu .row .text-left').map { |e| e.content.strip.delete "\n" }
       end
     end
   end
