8220d1fa28d65bfaf62447a16ed976ea45a4a125
Alexander Olofsson <alexander.olofsson@liu.se>
2019-04-05 10:38:18 +0200

    Fix collegium

--- lib/lunchplaner/backends/collegium.rb
+++ lib/lunchplaner/backends/collegium.rb
@@ -11,20 +11,24 @@ module Lunchplaner
 
       def data
         day = nil
+        cur = []
         raw_data.at_css('.meny:first').inner_html.split('<br>').each_with_object([]) do |e, a|
-          if day
-            if e.empty?
-              day = nil
-            else
-              a << e unless %w[HAV GÅRD ÄNG SALLAD].include? e
-            end
-          elsif e.start_with? '<strong>'
+          if e.start_with? '<strong>'
             if (e.include?('Måndag') && Time.now.monday?)\
             || (e.include?('Tisdag') && Time.now.tuesday?)\
             || (e.include?('Onsdag') && Time.now.wednesday?)\
             || (e.include?('Torsdag') && Time.now.thursday?)\
             || (e.include?('Fredag') && Time.now.friday?)
               day = true
+            end
+          elsif day
+            if e.empty? || e == e.upcase
+              a << cur.join(' ') unless cur.empty?
+              cur = []
+            elsif e.start_with? '<strong>'
+              day = nil
+            else
+              cur << e
             end
           end
         end
