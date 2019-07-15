885bf0580714d4ee76cf64386d4c993ead96fb9d
Alexander Olofsson <alexander.olofsson@liu.se>
2019-07-15 09:30:40 +0200

    Fix P2G

--- lib/lunchplaner/backends/p2g.rb
+++ lib/lunchplaner/backends/p2g.rb
@@ -1,10 +1,14 @@
 module Lunchplaner
   module Backends
     class P2g < Lunchplaner::Backend
-      url 'https://p2catering.se/'
+      url 'https://p2catering.se/lunchmeny/'
 
       def daily
-        data
+        data[:daily]
+      end
+
+      def weekly
+        data[:weekly]
       end
 
       def to_s
@@ -14,9 +18,27 @@ module Lunchplaner
       private
 
       def data
-        raw_data.at_css('.wpb_wrapper h3').parent.css('p').map do |e|
-          e.content.strip
-        end.reject(&:empty?)
+        daily = []
+        at_day = false
+        raw_data.at_css('.eltd-container-inner.clearfix').children.each do |e|
+          if e.name == 'h3'
+            at_day = false
+
+            if e.content.strip == 'Måndag' && Time.now.monday? ||
+               e.content.strip == 'Tisdag' && Time.now.tuesday? ||
+               e.content.strip == 'Onsdag' && Time.now.wednesday? ||
+               e.content.strip == 'Torsdag' && Time.now.thursday? ||
+               e.content.strip == 'Fredag' && Time.now.friday?
+              at_day = true
+            end
+          elsif at_day && e.name == 'p'
+            daily << e.content.strip
+          end
+        end
+
+        weekly = daily.pop(5)
+
+        { daily: daily, weekly: weekly }
       end
     end
   end
