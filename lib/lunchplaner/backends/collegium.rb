d782338c3bebfdc28513d2419afc2155d738fe7a
Alexander Olofsson <alexander.olofsson@liu.se>
2019-08-27 13:53:34 +0200

    Fix Collegium

--- lib/lunchplaner/backends/collegium.rb
+++ lib/lunchplaner/backends/collegium.rb
@@ -12,26 +12,38 @@ module Lunchplaner
       def data
         day = nil
         cur = []
-        raw_data.at_css('.column_page').css('p')[3..-3].each do |e|
+        result = []
+        raw_data.at_css('.column_page').css('p').each do |e|
           e = e.text
-          if (e.include?('Måndag') && Time.now.monday?)\
-            || (e.include?('Tisdag') && Time.now.tuesday?)\
-            || (e.include?('Onsdag') && Time.now.wednesday?)\
-            || (e.include?('Torsdag') && Time.now.thursday?)\
-            || (e.include?('Fredag') && Time.now.friday?)
-            day = true
-          elsif day
-            day = nil
-            if e.empty? || e == e.upcase
-              a << cur.join(' ') unless cur.empty?
-              cur = []
+          if %w[måndag tisdag onsdag torsdag fredag].include? e.strip.downcase
+            if (e.include?('Måndag') && Time.now.monday?)\
+              || (e.include?('Tisdag') && Time.now.tuesday?)\
+              || (e.include?('Onsdag') && Time.now.wednesday?)\
+              || (e.include?('Torsdag') && Time.now.thursday?)\
+              || (e.include?('Fredag') && Time.now.friday?)
+              day = true
             else
-              cur += e.split("\n").map { |entr| entr.split[1..-1].join ' ' }
+              day = false
+            end
+
+            result << cur.join(' ') unless cur.empty?
+            cur = []
+          elsif day
+            if e == e.upcase
+              result << cur.join(' ') unless cur.empty?
+              cur = []
+            elsif e.empty? || e.include?(':-') || e.include?('.-')
+              result << cur.join(' ') unless cur.empty?
+              cur = []
+
+              day = nil
+            else
+              cur += e.split("\n").reject(&:empty?)
             end
           end
         end
 
-        cur
+        result
       end
     end
   end
