6236afe66cc3522dcceb2b694f01188893a82708
Alexander Olofsson <alexander.olofsson@liu.se>
2019-10-14 16:26:28 +0200

    Add Fresh Market

--- lib/lunchplaner.rb
+++ lib/lunchplaner.rb
@@ -13,6 +13,7 @@ module Lunchplaner
   module Backends
     autoload :ChiliLime,           'lunchplaner/backends/chililime'
     autoload :Collegium,           'lunchplaner/backends/collegium'
+    autoload :FreshMarket,         'lunchplaner/backends/fresh_market'
     autoload :Husman,              'lunchplaner/backends/husman'
     autoload :Matkultur,           'lunchplaner/backends/matkultur'
     autoload :P2g,                 'lunchplaner/backends/p2g'
---
+++ lib/lunchplaner/backends/fresh_market.rb
@@ -0,0 +1,35 @@
+module Lunchplaner
+  module Backends
+    class FreshMarket < Lunchplaner::Backend
+      url 'https://www.ostgotakok.se/freshmarket/#meny'
+
+      def to_s
+        'Fresh Market by Östgötakök'
+      end
+
+      def daily
+        [data]
+      end
+
+      private
+
+      def data
+        weeknum = Time.now.strftime('%W').to_i + 1
+        in_week = false
+        raw_data.at_css('.menu--left-content .overflow').css('p').each do |e|
+          next if e.content.strip.empty?
+
+          if !in_week
+            in_week = true if e.content.end_with? weeknum.to_s
+          elsif (e.content.start_with?('Måndag') && Time.now.monday?) \
+             || (e.content.start_with?('Tisdag') && Time.now.tuesday?) \
+             || (e.content.start_with?('Onsdag') && Time.now.wednesday?) \
+             || (e.content.start_with?('Torsdag') && Time.now.thursday?) \
+             || (e.content.start_with?('Fredag') && Time.now.friday?)
+            return e.content.gsub(%r{^(Måndag|Tisdag|Onsdag|Torsdag|Fredag)|[\u202f\u00a0]}, '')
+          end
+        end
+      end
+    end
+  end
+end
