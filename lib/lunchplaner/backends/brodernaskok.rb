553a80300fb55821ad187a7ccd2c1cff4f0034dc
Alexander Olofsson <alexander.olofsson@liu.se>
2021-09-14 13:17:59 +0200

    Fix Brödernas Kök

--- lib/lunchplaner/backends/brodernaskok.rb
+++ lib/lunchplaner/backends/brodernaskok.rb
@@ -3,6 +3,10 @@ module Lunchplaner
     class BrodernasKok < Lunchplaner::Backend
       url 'https://brodernaskok.se/meny/'
 
+      def daily
+        data[:daily]
+      end
+
       def weekly
         data[:weekly]
       end
@@ -14,7 +18,18 @@ module Lunchplaner
       private
 
       def data
-        { weekly: raw_data.at_css('.menu-section').css('.menu-item-title').map(&:text) }
+        blocks = raw_data.css('.menu-section')
+
+        daily = blocks.select { |b| b.at_css('.menu-section-header').text.include? 'Dagens' }
+        weekly = blocks.find { |b| !b.at_css('.menu-section-header').text.include? 'Dagens' }
+
+        today = daily.map { |b| b.css('.menu-item-title').find { |i| i.text.include?(WEEKDAYS[Time.now.wday]) } }
+
+        d = { weekly: weekly.css('.menu-item-title').map(&:text) }
+        return d if today.empty?
+
+        d[:daily] = today.compact.map { |i| i.text.gsub(DAY_REX, '').gsub(' - ', '') }
+        d
       end
     end
   end
