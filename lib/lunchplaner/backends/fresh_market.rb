cfb8da088f3975f64a9f10593f6faa5648de8d47
Alexander Olofsson <alexander.olofsson@liu.se>
2021-09-24 12:53:21 +0200

    Parse Fresh Markets menu

--- lib/lunchplaner/backends/fresh_market.rb
+++ lib/lunchplaner/backends/fresh_market.rb
@@ -8,9 +8,7 @@ module Lunchplaner
       end
 
       def daily
-        [
-          'Dagens rätt',
-          'Dagens vegetariska',
+        data + [
           'Dagens soppa'
         ]
       end
@@ -25,23 +23,16 @@ module Lunchplaner
       private
 
       def data
-        weeknum = Time.now.strftime('%W').to_i + 1
-        in_week = false
-        raw_data.at_css('.menu--left-content .overflow').css('p').each do |e|
+        raw_data.at_css('.menu--left-content > .overflow').css('p').each do |e|
           next if e.content.strip.empty?
 
           curday = WEEKDAYS[Time.now.wday]
-          if !in_week
-            in_week = true if e.content.end_with? weeknum.to_s
-          elsif e.content.include?(curday)
-            return e.inner_html
-                    .gsub(%r{<br/?>}, "\n")
-                    .split("\n")
-                    .last
-                    .gsub(/Måndag|Tisdag|Onsdag|Torsdag|Fredag/, '')
-                    .gsub(%r{<[^<>]+/?>}, '')
-                    .strip
-          end
+          next unless e.at_css('b').content.include?(curday)
+
+          return e.children[1..]
+                  .text
+                  .split("\n")
+                  .map(&:strip)
         end
       end
     end
