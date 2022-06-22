4af9002cef528201f204685140ea29e27245bd0f
Alexander Olofsson <alexander.olofsson@liu.se>
2022-06-22 09:43:54 +0200

    Disable Kårallen during summer too

--- lib/lunchplaner.rb
+++ lib/lunchplaner.rb
@@ -22,7 +22,7 @@ module Lunchplaner
     # autoload :Falafelhuset,        'lunchplaner/backends/falafelhuset' # Konkurs 2022-06-02
     autoload :FreshMarket,         'lunchplaner/backends/fresh_market' if Time.now > Time.parse('2022-08-15')
     autoload :Husman,              'lunchplaner/backends/husman'
-    autoload :Karallen,            'lunchplaner/backends/karallen'
+    autoload :Karallen,            'lunchplaner/backends/karallen' if Time.now > Time.parse('2022-08-15')
     autoload :LaFontana,           'lunchplaner/backends/la_fontana'
     # autoload :P2g,                 'lunchplaner/backends/p2g'
     autoload :SpicyWokNRoll,       'lunchplaner/backends/spicy_wok_n_roll'
