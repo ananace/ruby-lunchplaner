12b70bac221e3f110fb35d693d8efdc75a8e4ec0
Alexander Olofsson <alexander.olofsson@liu.se>
2022-11-04 10:07:12 +0100

    Update husman's listing

--- lib/lunchplaner/backends/husman.rb
+++ lib/lunchplaner/backends/husman.rb
@@ -9,24 +9,12 @@ module Lunchplaner
         Time.now > Time.parse('2022-08-08')
       end
 
-      def links
-        # The official download URL scheme; (The restaurant doesn't always follow it)
-        # weeknum = Time.now.strftime('%W').to_i + 1
-        # weekly_url = "https://restauranghusman.se/wp-content/uploads/#{Time.now.strftime "%Y/%m/#{weeknum}-%y"}.pdf"
-
-        weekly_url = data.css('a.btn').first['href']
-
-        [
-          { href: weekly_url, type: 'far file-pdf', colour: 'primary', title: 'Hela veckans meny' }
-        ] + super
-      end
-
       def to_s
         'Restaurang Husman'
       end
 
       def daily
-        data.css('.todays-menu ul li').reject { |e| e.content =~ /V.lkommen!/ }.map do |e|
+        data.css('.jet-listing .e-con-full .e-con-boxed > .e-con-inner > .e-con-boxed span.elementor-heading-title').reject { |e| e.content =~ /V.lkommen!/ }.map do |e|
           e.content.strip.delete "\n"
         end
       end
