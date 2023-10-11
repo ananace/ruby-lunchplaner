7b032a6ef6a85ea72c10b644f4f92ed1c84189e4
Alexander Olofsson <alexander.olofsson@liu.se>
2023-10-11 10:41:08 +0200

    Fix fresh market

--- lib/lunchplaner/backends/fresh_market.rb
+++ lib/lunchplaner/backends/fresh_market.rb
@@ -31,7 +31,7 @@ module Lunchplaner
       private
 
       def data
-        content = raw_data.at_css('#menyblock').css('h3 ~ p, h4 ~ p').map(&:content)
+        content = raw_data.at_css('div:has(#menyblock)').css('h3 ~ p, h4 ~ p').map(&:content)
         content[Time.now.wday - 1]
       end
     end
