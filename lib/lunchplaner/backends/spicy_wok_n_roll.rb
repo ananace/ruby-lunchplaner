bcef2a8828e7a3429656977c030ceb5ebf1ad03a
Alexander Olofsson <alexander.olofsson@liu.se>
2024-08-23 11:12:56 +0200

    Fix Spicy Wok'n'Roll

--- lib/lunchplaner/backends/spicy_wok_n_roll.rb
+++ lib/lunchplaner/backends/spicy_wok_n_roll.rb
@@ -29,10 +29,11 @@ module Lunchplaner
           daily = nil
           weekly = blocks.first.css('.product').map { |b| b.at_css('span').text.strip }
         else
-          weekly = blocks.first.css('.product').map { |b| b.at_css('span').text.strip }
+          weekly = blocks.find { |b| b.at_css('h2#veckans-erbjudanden') }
+                         .css('.product').map { |b| b.at_css('span').text.strip }
           daily = blocks.find { |b| b.at_xpath(".//div[contains(@class,\"summary\")]/h2[contains(text(),#{WEEKDAYS[Time.now.wday].inspect})]") }
                         .xpath('.//h3/span[not(@class)]')
-                        .map { |b| b.text =~ /\Abuffe\Z/i ? b.text.strip.capitalize : b.text.sub(/^\s*\w\.?\s*/, '').strip }
+                        .map { |b| b.text =~ /\Abuff.\Z/i ? b.text.strip.capitalize : b.text.sub(/^\s*\w\.?\s*/, '').strip }
                         .reject(&:empty?)
         end
 
